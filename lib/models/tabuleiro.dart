4140593954134aac21d65122febd93981b2b359d
Bruno Machado <brunomachado3818@gmail.com>
2023-05-02 15:53:24 -0300

    implementado classe Tabuleiro

---
+++ lib/models/tabuleiro.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+
+import 'package:campo_minado/models/campo.dart';
+import 'package:flutter/cupertino.dart';
+
+class Tabuleiro {
+  final int linhas;
+  final int colunas;
+  final int qtdeBombas;
+
+  final List<Campo> _campos = [];
+
+  Tabuleiro({@required this.linhas, @required this.colunas, @required this.qtdeBombas}) {
+    _criarCampos();
+    _relacionarVizinhos();
+    _sortearMinas();
+  }
+
+  void reiniciar(){
+    _campos.forEach((c)=>c.reiniciar());
+    _sortearMinas();
+  }
+
+  void revelarBombas(){
+    _campos.forEach((c)=>c.revelarBomba());
+  }
+
+  void _criarCampos() {
+    for (int x = 0; x < linhas; x++) {
+      for (int y = 0; y < colunas; y++) {
+        _campos.add(Campo(linha: x, coluna: y));
+      }
+    }
+  }
+
+  void _relacionarVizinhos() {
+    for (var campo in _campos) {
+      for (var vizinho in _campos) {
+        campo.adicionarVizinho(vizinho);
+      }
+    }
+  }
+
+  void _sortearMinas() {
+    int sorteadas = 0;
+
+    if(qtdeBombas > linhas*colunas){
+      return;
+    }
+
+    while (sorteadas < qtdeBombas) {
+      int i = Random().nextInt(_campos.length);
+      if (!_campos[i].minado) {
+        sorteadas++;
+        _campos[i].minar();
+      }
+    }
+  }
+
+  List<Campo> get campos{
+    return _campos;
+  }
+
+  bool get resolvido{
+    return _campos.every((c)=>c.resolvido);
+  }
+
+}
